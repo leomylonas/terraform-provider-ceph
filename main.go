7d321478f57329c44058097e45731b3ba503ba09
Leo Mylonas <leo@mylonas.co>
2025-01-28 16:32:16 +1000

    Update address

--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 package main
 
 import (
@@ -17,9 +14,6 @@ var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary.
 	version string = "dev"
-
-	// goreleaser can pass other information to the main package, such as the specific commit
-	// https://goreleaser.com/cookbooks/using-main.version/
 )
 
 func main() {
@@ -29,10 +23,7 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
-		Address: "github.com/leomylonas/terraform-provider-ceph",
+		Address: "leomylonas/ceph",
 		Debug:   debug,
 	}
 
